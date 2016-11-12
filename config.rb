602cf81b3ccdb6457e116e49da5b84f9ae78f2de
Claudio Vallejo <hola@claudiovallejo.mx>
2016-11-12 07:28:36 -0600

    Cleaned up config.rb.

--- config.rb
+++ config.rb
@@ -1,4 +1,19 @@
 ###
+# General configuration
+###
+
+# DatoCMS Extension
+activate :dato,
+  domain: 'morning-mountain-8323.admin.datocms.com',
+  token: 'caa4b8f93f5afe9453a37f15022b74ae227ce8e9f62814eb99',
+  base_url: 'http://www.materialesvallejo.com'
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+###
 # Page options, layouts, aliases and proxies
 ###
 
@@ -9,42 +24,7 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+#
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
-
-# General configuration
-
-# Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
-
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-# Build-specific configuration
-configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-end
-
-# DatoCMS Extension
-activate :dato,
-  domain: 'morning-mountain-8323.admin.datocms.com',
-  token: 'caa4b8f93f5afe9453a37f15022b74ae227ce8e9f62814eb99',
-  base_url: 'http://www.materialesvallejo.com'
